8f655e79140d6697a551731edefa1e037d8d98ee
korolkovskreiss <113632378+korolkovskreiss@users.noreply.github.com>
2023-02-08 01:28:36 +0200

    Init: add test code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function toReadable (number) {
-  
+    const numbersDec = ['zero','one','two','three','four','five','six','seven','eight','nine'];
+  const numbersTeen = ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
+  const numbersTy = ['twenty','thirty','forty','fifty',
+'sixty','seventy','eighty','ninety'];
+  let result = []
+	const numberStr = number.toString()
+  if (numberStr <= 9 ) {
+  	numbersDec.forEach((val, id) => {
+    	if (id === number){
+      	result = val;
+      }
+    })
+  }
+  else if (a){}
+  return result;
 }
