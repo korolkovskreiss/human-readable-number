2cdffb4b69217e4d102c25fa6733871d10ba3b42
korolkovskreiss <113632378+korolkovskreiss@users.noreply.github.com>
2023-02-09 01:21:15 +0200

    Init: add compleated code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,78 @@
 module.exports = function toReadable (number) {
     const numbersDec = ['zero','one','two','three','four','five','six','seven','eight','nine'];
   const numbersTeen = ['ten','eleven','twelve','thirteen', 'fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
-  const numbersTy = ['twenty','thirty','forty','fifty',
+  const numbersTy = ['','','twenty','thirty','forty','fifty',
 'sixty','seventy','eighty','ninety'];
-  let result = []
+  let result = ""
 	const numberStr = number.toString()
-  if (numberStr <= 9 ) {
+  if (number <= 9) {
   	numbersDec.forEach((val, id) => {
     	if (id === number){
       	result = val;
       }
     })
   }
-  else if (a){}
+  else if (number <= 19){
+  	numbersTeen.forEach((val, id) => {
+    	if (id + 10 === number){
+      	result = val;
+      }
+    })
+  }
+  else if (number <= 99) {
+  	if ( Number(numberStr[1]) === 0 ) {
+  		numbersTy.forEach((val, id) => {
+    		if (id * 10 === number){
+          result = val;
+        }
+    	})
+  	}
+  	else { 
+    	numbersTy.forEach((val, id) => {
+    		if (id  === Number(numberStr[0])){
+          result = val + ' ';
+        }
+    	})
+      numbersDec.forEach((val, id) => {
+    		if (id === Number(numberStr[1])){
+          result += val;
+        }
+      })
+    }
+	}
+  else {
+    	numbersDec.forEach((val, id) => {
+    		if ( id === Number(numberStr[0])) {
+      		result = val +" hundred";}})
+          
+      if ( Number(numberStr[1]) === 0 && Number(numberStr[2]) <= 9 && Number(numberStr[2]) !== 0){
+         numbersDec.forEach((val, id) => {
+            if (id === Number(numberStr[2])){
+              result += " " + val;
+     		 	}
+  		  })
+       }
+    	else if ( Number(numberStr[1]) !== 0 && Number(numberStr[1]) * 10 + Number(numberStr[2]) <= 19 ) {
+      		numbersTeen.forEach((val, id) => {
+            if (id + 10 === Number(numberStr[1]) * 10 + Number(numberStr[2])){
+              result += " " + val;
+     		 	}
+    		})
+      }
+      else if( Number(numberStr[1]) !== 0 && Number(numberStr[1]) * 10 + Number(numberStr[2]) > 19){
+          numbersTy.forEach((val, id) => {
+                  if (id === Number(numberStr[1])){
+                    result += " " + val;
+          }
+       })
+       		if (Number(numberStr[2]) > 0){
+       			numbersDec.forEach((val, id) => {
+            if (id === Number(numberStr[2])){
+              result += " " + val;
+            }
+          })
+       }
+    }
+  }
   return result;
 }
